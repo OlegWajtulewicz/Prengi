b0130e4e49ce54ef84e024c646000c2630b721e3
aleh vaitulevich <vaitulevichaleh@gmail.com>
2023-03-28 14:27:44 +0200

    tabs

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -19,9 +19,13 @@ $(document).ready(function(){
 	$('.carousel__inner').slick({
 		dots: true,
 		speed: 1800,
-		autoplay: true,
+		autoplay: false,
   		autoplaySpeed: 4000,
 		  prevArrow: '<button type="button" class="slick-prev"><img src="../img/left.png"></button>',
 		  nextArrow: '<button type="button" class="slick-next"><img src="../img/right.png"></button>'
 	});
   });
+
+  
+
+  
---
+++ src/js/slider.js
@@ -0,0 +1,11 @@
+// $(document).ready(function(){
+// 	$('.carousel__inner-solution').slick({
+// 		dots: true,
+// 		speed: 1800,
+// 		autoplay: false,
+//   		autoplaySpeed: 4000,
+// 		  prevArrow: '<button type="button" class="slick-prev"><img src="../img/left.png"></button>',
+// 		  nextArrow: '<button type="button" class="slick-next"><img src="../img/right.png"></button>'
+// 	});
+//   });
+   
